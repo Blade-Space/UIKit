65d04e6cae80b8d32eddf1706091588ab5792bb0
egorkaBurkenya <sleepyegorka@gmail.com>
2022-10-27 20:56:24 +0300

    code refactor

--- src/types/hooksTypes.ts
+++ src/types/hooksTypes.ts
@@ -1,6 +1,3 @@
 import React from "react"
 
-
-type EventTargetValue = React.ChangeEvent<HTMLInputElement>
-//FIXME: Надо разобраться с типизацией. параметра отвечающего за изменение value
 export type InputDispatchSetState = ((arg0: string) => void)
