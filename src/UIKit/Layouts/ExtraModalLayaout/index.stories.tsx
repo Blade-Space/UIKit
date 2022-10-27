45480ef700f75b3dcac6604b4c643ee8e082708a
egorkaBurkenya <sleepyegorka@gmail.com>
2022-10-28 01:56:14 +0300

    add extra modal layaout 🔥

--- src/UIKit/Layouts/ExtraModalLayaout/index.stories.tsx
+++ src/UIKit/Layouts/ExtraModalLayaout/index.stories.tsx
@@ -1,13 +1,187 @@
 import * as React from "react";
 import { Meta, Story } from "@storybook/react";
 import ExtraModalLayaout from "./index";
-import {Props} from './index'; 
+import {ExtraModalLayaoutProps} from './index'; 
+import Text from "../../Text";
 
 export default {
   component: ExtraModalLayaout,
 } as Meta;
 
-export const Default: Story<Props> = (args) => <ExtraModalLayaout {...args} />;
-Default.args = {};
+export const Default: Story<ExtraModalLayaoutProps> = (args) => <ExtraModalLayaout {...args} />;
+Default.args = {
+  children: "",
+  items: [{
+      type: "item",
+      options: {
+        children: "Open",
+        onClick: () => {console.log("Open")}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "Edit",
+        onClick: () => {console.log("Edit")}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "Rename",
+        onClick: () => {console.log("Rename")}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "Delete",
+        onClick: () => {console.log("Delete")}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "Copy full path",
+        onClick: () => {console.log("Copy full path")}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "Info",
+        onClick: () => {console.log("Info")}
+      }
+    },
+    {type: "line"},
+    {
+      type: "item",
+      options: {
+        children: "Open in VSCode online",
+        onClick: () => {console.log("Open in VSCode online")},
+        Icon: {
+          type: "svg",
+          url: "assets\\vscode-small.svg"
+        }
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "Download on your PC",
+        onClick: () => {console.log("Download on your PC")},
+        Icon: {
+          type: "svg",
+          url: "assets\\download-small.svg"
+        }
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "View in git repo",
+        onClick: () => {console.log("View in git repo")},
+        Icon: {
+          type: "svg",
+          url: "assets\\github-small.svg"
+        }
+      }
+    }]
+};
+
+
+export const WithDescription: Story<ExtraModalLayaoutProps> = (args) => <ExtraModalLayaout {...args} />;
+WithDescription.args = {
+  children: "",
+  description: {
+    text: "from google fonts",
+    link: "/"
+  },
+  items: [{
+      type: "item",
+      options: {
+        children: "Open",
+        onClick: () => {console.log("Open")}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "Edit",
+        onClick: () => {console.log("Edit")}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "Rename",
+        onClick: () => {console.log("Rename")}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "Delete",
+        onClick: () => {console.log("Delete")}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "Copy full path",
+        onClick: () => {console.log("Copy full path")}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "Info",
+        onClick: () => {console.log("Info")}
+      }
+    },
+    {type: "line"},
+    {
+      type: "item",
+      options: {
+        children: "Open in VSCode online",
+        onClick: () => {console.log("Open in VSCode online")},
+        Icon: {
+          type: "svg",
+          url: "assets\\vscode-small.svg"
+        }
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "Download on your PC",
+        onClick: () => {console.log("Download on your PC")},
+        Icon: {
+          type: "svg",
+          url: "assets\\download-small.svg"
+        }
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "View in git repo",
+        onClick: () => {console.log("View in git repo")},
+        Icon: {
+          type: "svg",
+          url: "assets\\github-small.svg"
+        }
+      }
+    }]
+};
+
+export const WithChildren: Story<ExtraModalLayaoutProps> = (args) => <ExtraModalLayaout {...args} />;
+WithChildren.args = {
+  children: <div style={{marginBottom: "12px"}}><Text>Надо придумать как передавать шрифт в компонент Text</Text></div>,
+  description: {
+    text: "from google fonts",
+    link: "/"
+  }
+};
 
 
--- src/UIKit/Layouts/ExtraModalLayaout/index.tsx
+++ src/UIKit/Layouts/ExtraModalLayaout/index.tsx
@@ -1,15 +1,50 @@
 import React from 'react';
+import ExtraModalItem, {ExtraModalItemProps} from './ExtraModalItem';
 import styles from './index.module.css';
 
-export interface Props {
-    children: JSX.Element 
+import Text from '../../Text';
+import SmallDescriptonLink from './SmallDescriptonLink';
+
+type ExtraModalLayaoutItem = {
+    type: "item" | "line"
+    options?: ExtraModalItemProps
 }
 
-const ExtraModalLayaout = ({children}: Props) => {
+export interface ExtraModalLayaoutProps {
+    children?: React.ReactNode
+    items?: ExtraModalLayaoutItem[]
+    description?: {
+        text: string
+        link: string
+    } 
+}
+
+const ExtraModalLayaout = ({children, items, description}: ExtraModalLayaoutProps) => {
     return (
-        <div>
-           {children} 
-           h1 title
+        <div className={styles.modal}>
+            {children && children}
+            {items && items.map((item) => 
+                item.type == "line" ? 
+                    <div className={styles.description__line} /> 
+                    : 
+                    item.type == "item" &&
+                    <ExtraModalItem
+                        className={styles.item}
+                        Icon={item.options?.Icon} 
+                        onClick={item.options!.onClick}>
+                        {item.options!.children}
+                    </ExtraModalItem>
+                    )}
+            {
+                description &&
+                    <div className={styles.description}>
+                        <div className={styles.description__line} /> 
+                        <div className={styles.description__content}>
+                            <Text size="small 14px" color="gray">{description.text}</Text>
+                            <SmallDescriptonLink link={description.link} />
+                        </div>
+                    </div>
+            }
         </div>
     );
 };
