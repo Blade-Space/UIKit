d5617ad2cdc0ca8ac3fd0b9b40e6904fbafcc857
egorkaBurkenya <sleepyegorka@gmail.com>
2022-10-28 05:28:35 +0300

    add dropdawn 🔥

--- src/UIKit/Dropdown/index.stories.tsx
+++ src/UIKit/Dropdown/index.stories.tsx
@@ -1,13 +1,154 @@
 import * as React from "react";
 import { Meta, Story } from "@storybook/react";
 import Dropdown from "./index";
-import {Props} from './index'; 
+import {DropdownProps} from './index'; 
 
 export default {
   component: Dropdown,
 } as Meta;
 
-export const Default: Story<Props> = (args) => <Dropdown {...args} />;
-Default.args = {};
+export const Default: Story<DropdownProps> = (args) => <Dropdown {...args} />;
+Default.args = {
+  defaultValue: "default",
+  onChange: (value) => {console.log("value: "+value+" was changed")},
+  width: 120,
+  items: [{
+      type: "item",
+      options: {
+        children: "option 1",
+        onClick: () => {}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "option 2",
+        onClick: () => {}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "option 3",
+        onClick: () => {}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "option 4",
+        onClick: () => {}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "option 5",
+        onClick: () => {}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "option 6",
+        onClick: () => {}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "option 7",
+        onClick: () => {},
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "option 8",
+        onClick: () => {},
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "option 9",
+        onClick: () => {},
+        }
+    }],
+};
+
+export const WithDescription: Story<DropdownProps> = (args) => <Dropdown {...args} />;
+WithDescription.args = {
+  defaultValue: "default",
+  description: {
+    text: "from google fonts",
+    link: "/"
+  },
+  onChange: (value) => {console.log("value: "+value+" was changed")},
+  width: 230,
+  items: [{
+      type: "item",
+      options: {
+        children: "option 1",
+        onClick: () => {}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "option 2",
+        onClick: () => {}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "option 3",
+        onClick: () => {}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "option 4",
+        onClick: () => {}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "option 5",
+        onClick: () => {}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "option 6",
+        onClick: () => {}
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "option 7",
+        onClick: () => {},
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "option 8",
+        onClick: () => {},
+      }
+    },
+    {
+      type: "item",
+      options: {
+        children: "option 9",
+        onClick: () => {},
+        }
+    }],
+};
 
 
--- src/UIKit/Dropdown/index.tsx
+++ src/UIKit/Dropdown/index.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './index.module.css';
 
-export interface Props {
+import Text from '../Text';
+import DropdownIcon from './DropdownIcon';
+import ExtraModalLayaout, {ExtraModalLayaoutItem} from '../Layouts/ExtraModalLayaout';
 
+export interface DropdownProps {
+    defaultValue: string
+    items: ExtraModalLayaoutItem[]
+    description?: {
+        text: string
+        link: string
+    },
+    width?: number
+    onChange: (value: React.ReactNode) => void;
 }
 
-const Dropdown = ({}: Props) => {
+const Dropdown = ({defaultValue, items, description, onChange, width=230}: DropdownProps) => {
+
+    const [isActive, setIsActive] = useState(false);
+    const [_default, set_Default] = useState<React.ReactNode>(defaultValue); 
+
+    const changeHandler = (value: React.ReactNode) => {
+        onChange(value);
+        set_Default(value);
+    }
+
+    items.map((item) => {
+        if (item.type === "item") {
+            item.options!.onClick = () => {changeHandler(item.options!.children)}
+        }
+    })
+
     return (
-        <div>
-            
+        <div className={styles.dropdown} style={{width}}>
+            <div onClick={() => setIsActive(prev=>!prev)} className={styles.dropdown__select_item}>
+                <Text>{_default}</Text>
+                <DropdownIcon isActive={isActive} />
+            </div>
+            {isActive && 
+                <ExtraModalLayaout 
+                    description={description}
+                    width={width}
+                    items={items} 
+                    className={styles.dropdown__extra_modal} />}
         </div>
     );
 };
