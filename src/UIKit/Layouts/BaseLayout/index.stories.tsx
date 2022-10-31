3240f3920956450c99407efe3985462d6c6a5efd
egorkaBurkenya <sleepyegorka@gmail.com>
2022-11-01 02:11:58 +0300

    add BaseLayout without close, hiden ⚠️

--- src/UIKit/Layouts/BaseLayout/index.stories.tsx
+++ src/UIKit/Layouts/BaseLayout/index.stories.tsx
@@ -1,13 +1,19 @@
 import * as React from "react";
 import { Meta, Story } from "@storybook/react";
 import BaseLayout from "./index";
-import {Props} from './index'; 
+import {BaseLayoutProps} from './index'; 
+
+import Text from '../../Text';
 
 export default {
   component: BaseLayout,
 } as Meta;
 
-export const Default: Story<Props> = (args) => <BaseLayout {...args} />;
-Default.args = {};
+export const Default: Story<BaseLayoutProps> = (args) => <BaseLayout {...args} />;
+Default.args = {
+  onClose: () => {alert("close")},
+  size: {_width: "700px", _height: "300px"},
+  title: "Default Layout",
+  children: <Text>Hello world</Text>
 
-
+};
--- src/UIKit/Layouts/BaseLayout/index.tsx
+++ src/UIKit/Layouts/BaseLayout/index.tsx
@@ -1,15 +1,78 @@
 import React from 'react';
 import styles from './index.module.css';
 
-export interface Props {
-    children: JSX.Element 
+import BaseLayoutHeader from './BaseLayoutHeader';
+
+export interface BaseLayoutProps {
+    children: React.ReactNode
+
+    onClose?: () => void
+    onHide: () => void
+
+    Scaling?: boolean;
+
+    title: string
+
+    size: {
+        _width: string
+        _height: string
+    }
 }
 
-const BaseLayout = ({children}: Props) => {
+const BaseLayout = (
+    {
+
+        children, 
+        onClose, 
+        onHide,
+        size: {_width="420px", _height="310px"}, 
+        title,
+        Scaling=false
+
+    }: BaseLayoutProps) => {
+
+        const [width, setWidth] = React.useState(_width)
+        const [height, setHeight] = React.useState(_height)
+        const [isScaling, setIsScaling] = React.useState(false);
+
+        React.useEffect(() => {
+            switch (isScaling) {
+                case true:
+                    setWidth("100%")
+                    setHeight("100vh")
+                    break;
+                case false:
+                    setWidth(_width)
+                    setHeight(_height)
+                    break;
+            
+                default:
+                    break;
+            }
+        }, [_height, _width, isScaling])
+
+        const ScalingHandler = () => {
+            setIsScaling((prev)=>!prev)
+        }
+
     return (
+
         <div>
-           {children} 
+            <BaseLayoutHeader 
+                title={title} 
+                width={width}
+                onClose={onClose} 
+                onHide={onHide}
+                onScale={ScalingHandler}
+                />
+            <div
+                style={{height, width}}
+                className={styles.container}
+            >
+                {children} 
+            </div>
         </div>
+
     );
 };
 
