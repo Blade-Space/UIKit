c9de1825154bda6c08f79ed3cf5dba3d7145a11c
egorkaBurkenya <sleepyegorka@gmail.com>
2022-11-02 06:17:17 +0300

    finish deafult layout component

--- src/UIKit/Layouts/BaseLayout/index.stories.tsx
+++ src/UIKit/Layouts/BaseLayout/index.stories.tsx
@@ -9,11 +9,159 @@ export default {
   component: BaseLayout,
 } as Meta;
 
-export const Default: Story<BaseLayoutProps> = (args) => <BaseLayout {...args} />;
+const styleForDiv = {
+  width: "100%", 
+  height: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center"
+}
+
+export const Default: Story<BaseLayoutProps> = (args) => {
+  const [zBiggest, _setZBiggest] = React.useState(1);
+  
+  const setZBiggest = (n: number) => {
+    _setZBiggest(n)
+  }
+return ( 
+    <BaseLayout {...args} Scaling BiggestValueOfTheLayer={{value:zBiggest, setValue: setZBiggest}} />
+  )
+};
 Default.args = {
   onClose: () => {alert("close")},
-  size: {_width: "700px", _height: "300px"},
+  size: {width: "700px", height: "300px"},
   title: "Default Layout",
-  children: <Text>Hello world</Text>
+  children: <div style={styleForDiv}><Text size="large 24px">Default Window⚙️</Text></div>,
+  draggable: true,
+  openPosition: {x: 50, y: 0},
+  onHide: () => {alert("onHide")}
+};
 
+
+interface BaseLayoutPropsForThreeWindows {
+  first: BaseLayoutProps
+  second: BaseLayoutProps
+  three: BaseLayoutProps
+}
+
+export const ThreeWindows: Story<BaseLayoutPropsForThreeWindows> = (args) => {
+  const [zBiggest, _setZBiggest] = React.useState(1);
+  
+  const setZBiggest = (n: number) => {
+    console.log(n);
+    _setZBiggest(n)
+  }
+return ( 
+  <div>
+    <BaseLayout 
+      Scaling
+      BiggestValueOfTheLayer={{ value: zBiggest, setValue: setZBiggest }} 
+      title={"window1"}  
+      onHide={() => {alert("onHide")}} 
+      onClose={() => {alert("onClose")}} 
+      size={{
+        width: "700px",
+        height: "500px"
+      }} 
+      openPosition={{
+        x: 0,
+        y: 0
+      }}>
+        <div style={styleForDiv}><Text size="large 24px">Window 1🥱</Text></div>
+    </BaseLayout>
+    <BaseLayout 
+      Scaling
+      BiggestValueOfTheLayer={{ value: zBiggest, setValue: setZBiggest }} 
+      title={"window2"}  
+      onHide={() => {alert("onHide")}} 
+      onClose={() => {alert("onClose")}} 
+      size={{
+        width: "400px",
+        height: "200px"
+      }} 
+      openPosition={{
+        x: 200,
+        y: 100
+      }}>
+        <div style={styleForDiv}><Text size="large 24px">Window 2🔥</Text></div>
+    </BaseLayout>
+    <BaseLayout 
+      Scaling
+      BiggestValueOfTheLayer={{ value: zBiggest, setValue: setZBiggest }} 
+      title={"window3"}  
+      onHide={() => {alert("onHide")}} 
+      onClose={() => {alert("onClose")}} 
+      size={{
+        width: "500px",
+        height: "100px"
+      }} 
+      openPosition={{
+        x: 300,
+        y: 300
+      }}>
+        <div style={styleForDiv}><Text size="large 24px">Window 3🤍</Text></div>
+    </BaseLayout>
+  </div>
+  )
+};
+ThreeWindows.args = {};
+
+export const WithOutScale: Story<BaseLayoutProps> = (args) => {
+  const [zBiggest, _setZBiggest] = React.useState(1);
+  
+  const setZBiggest = (n: number) => {
+    _setZBiggest(n)
+  }
+return ( 
+    <BaseLayout {...args} BiggestValueOfTheLayer={{value:zBiggest, setValue: setZBiggest}} />
+  )
+};
+WithOutScale.args = {
+  onClose: () => {alert("close")},
+  size: {width: "700px", height: "300px"},
+  title: "Default Layout",
+  children: <div style={styleForDiv}><Text size="large 24px">Default Window⚙️</Text></div>,
+  draggable: true,
+  openPosition: {x: 50, y: 0},
+  onHide: () => {alert("onHide")}
+};
+
+export const WithOutButtons: Story<BaseLayoutProps> = (args) => {
+  const [zBiggest, _setZBiggest] = React.useState(1);
+  
+  const setZBiggest = (n: number) => {
+    _setZBiggest(n)
+  }
+return ( 
+    <BaseLayout {...args} BiggestValueOfTheLayer={{value:zBiggest, setValue: setZBiggest}} />
+  )
+};
+WithOutButtons.args = {
+  onClose: undefined,
+  onHide: undefined,
+  size: {width: "700px", height: "300px"},
+  title: "Default Layout",
+  children: <div style={styleForDiv}><Text size="large 24px">Default Window⚙️</Text></div>,
+  draggable: true,
+  openPosition: {x: 50, y: 0}
+};
+
+export const NotDraggable: Story<BaseLayoutProps> = (args) => {
+  const [zBiggest, _setZBiggest] = React.useState(1);
+  
+  const setZBiggest = (n: number) => {
+    _setZBiggest(n)
+  }
+return ( 
+    <BaseLayout {...args} Scaling BiggestValueOfTheLayer={{value:zBiggest, setValue: setZBiggest}} />
+  )
+};
+NotDraggable.args = {
+  onClose: () => {alert("close")},
+  size: {width: "700px", height: "300px"},
+  title: "Default Layout",
+  children: <div style={styleForDiv}><Text size="large 24px">Default Window⚙️</Text></div>,
+  draggable: false,
+  openPosition: {x: 50, y: 0},
+  onHide: () => {alert("onHide")}
 };
--- src/UIKit/Layouts/BaseLayout/index.tsx
+++ src/UIKit/Layouts/BaseLayout/index.tsx
@@ -1,79 +1,153 @@
 import React from 'react';
 import styles from './index.module.css';
 
+import { useDrag } from '@use-gesture/react';
+import { useSpring, animated  } from '@react-spring/web'
+
 import BaseLayoutHeader from './BaseLayoutHeader';
 
 export interface BaseLayoutProps {
-    children: React.ReactNode
-
-    onClose?: () => void
-    onHide: () => void
-
+    /**Window title */
+    title: string;
+    /** content rendering in layout */
+    children: React.ReactNode;
+    /**func for closing the window*/
+    onClose?: () => void;
+    /**func for hide the window*/
+    onHide?: () => void;
+    /** */
+    /**Permission to scale */
     Scaling?: boolean;
-
-    title: string
-
+    /**Permission to draggable */
+    draggable?: boolean;
+    /**default window sizes */
     size: {
-        _width: string
-        _height: string
+        width: string;
+        height: string;
+    };
+    /** the initial position of the open window */
+    openPosition: {
+        x: number; 
+        y: number;
+    };
+    /**the highest value of the layer at the moment and the function for changing it */
+    BiggestValueOfTheLayer: {
+        value: number;
+        setValue: (value: number) => void;
     }
 }
 
 const BaseLayout = (
     {
-
         children, 
-        onClose, 
-        onHide,
-        size: {_width="420px", _height="310px"}, 
+        onClose=undefined, 
+        onHide=undefined,
+        size: {width="420px", height="310px"}, 
         title,
-        Scaling=false
-
+        Scaling=false,
+        draggable,
+        openPosition,
+        BiggestValueOfTheLayer
     }: BaseLayoutProps) => {
 
-        const [width, setWidth] = React.useState(_width)
-        const [height, setHeight] = React.useState(_height)
-        const [isScaling, setIsScaling] = React.useState(false);
+        const [{x, y}, api] = useSpring(() => ({ x: openPosition.x, y: openPosition.y }))
 
-        React.useEffect(() => {
-            switch (isScaling) {
-                case true:
-                    setWidth("100%")
-                    setHeight("100vh")
-                    break;
-                case false:
-                    setWidth(_width)
-                    setHeight(_height)
-                    break;
-            
-                default:
-                    break;
-            }
-        }, [_height, _width, isScaling])
+        const [windowSize, setWindowSize] = React.useState(getWindowSize());                        // user window inner size {innerWidth, innerHeight} = window
+        const [ownSize, setOwnSize] = React.useState({width, height})                               // layout own size
+        const [cordinates, setCordinates] = React.useState({x: openPosition.x , y: openPosition.y}) // save the coordinates, to return to the places after the cancellation of scaling
+        const [isScaling, setIsScaling] = React.useState(false);                                    // is the window Scaling
+        const [isDragAllow, setIsDragAllow] = React.useState(draggable);                            // is it allowed to move the layout on the screen
+        const [zIndex, setZIndex] = React.useState(BiggestValueOfTheLayer.value);                   // layout layer
+        
+        /** **Function for moving around the screen** */
+        const bindWindowPos = useDrag(                                                              
+            (params) => {   
+                if (zIndex <= BiggestValueOfTheLayer.value) {
+                    setZIndex(BiggestValueOfTheLayer.value + 2)
+                    BiggestValueOfTheLayer.setValue(BiggestValueOfTheLayer.value + 1)              
+                }
+                setCordinates({
+                    x: openPosition.x + params.offset[0],
+                    y: openPosition.y + params.offset[1]
+                })  
+                api.start({
+                    x: openPosition.x + params.offset[0],
+                    y: openPosition.y + params.offset[1],
+                    immediate: true,  
+                })
+        },
+        {bounds: { 
+            left: -openPosition.x, 
+            right: windowSize.innerWidth - openPosition.x - parseInt(width.split("px")[0]) - 2, 
+            top: -openPosition.y, 
+            bottom: windowSize.innerHeight - openPosition.y - parseInt(height.split("px")[0]) - 36
+        },
+        enabled: draggable === false ? false : isDragAllow
+    });
 
+        /** **Сhanging the scale state** */
         const ScalingHandler = () => {
             setIsScaling((prev)=>!prev)
         }
 
+        React.useEffect(() => {
+            switch (isScaling) {
+                case true:
+                    api.start({x: 0, y: 0, config:{duration: 200}})
+                    setOwnSize({width: "99vw", height: "90vh"})
+                    setIsDragAllow(false)
+                    break;
+                case false:
+                    api.start({x: cordinates.x,  y: cordinates.y, config:{duration: 200}})
+                    setOwnSize({width, height})
+                    setIsDragAllow(true)
+                    break;
+                default:
+                    break;
+            }
+        }, [height, width, api, cordinates.x, cordinates.y, isScaling]) 
+        
+        React.useEffect(() => {
+            function handleWindowResize() {
+              setWindowSize(getWindowSize());
+            }
+        
+            window.addEventListener('resize', handleWindowResize);
+        
+            return () => {
+              window.removeEventListener('resize', handleWindowResize);
+            };
+          }, []);
+
     return (
 
-        <div>
+        <animated.div 
+            className={styles.container} 
+            style={{
+                position: 'absolute',
+                left: x,
+                top: y,
+                zIndex: zIndex
+        }}>
             <BaseLayoutHeader 
+                dragHandlers={bindWindowPos}
                 title={title} 
-                width={width}
+                width={ownSize.width}
                 onClose={onClose} 
                 onHide={onHide}
-                onScale={ScalingHandler}
+                onScale={Scaling ? ScalingHandler : undefined}
                 />
-            <div
-                style={{height, width}}
-                className={styles.container}
-            >
+            <div style={{height: ownSize.height, width: ownSize.width}} className={styles.content}>
                 {children} 
             </div>
-        </div>
+        </animated.div>
 
     );
 };
+
+function getWindowSize() {
+    const {innerWidth, innerHeight} = window;
+    return {innerWidth, innerHeight};
+  }
 
 export default BaseLayout;
