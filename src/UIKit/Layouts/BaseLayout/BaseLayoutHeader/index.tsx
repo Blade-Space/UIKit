3f71b75791cd514f27d1dda0cacea241740f8287
egorkaBurkenya <sleepyegorka@gmail.com>
2022-11-02 06:16:53 +0300

    cod refactor, add descrip, making work correct✅

--- src/UIKit/Layouts/BaseLayout/BaseLayoutHeader/index.tsx
+++ src/UIKit/Layouts/BaseLayout/BaseLayoutHeader/index.tsx
@@ -1,43 +1,41 @@
+import { ReactDOMAttributes } from '@use-gesture/react/dist/declarations/src/types';
 import React from 'react';
 import Text from '../../../Text';
 import HeaderCircle from './HeaderCircle';
 import styles from './index.module.css';
 
-
-
-// import { Range } from '../../../../types/rangeType';
-
 export interface BaseLayoutHeaderProps {
-    onClose?: React.MouseEventHandler<HTMLParagraphElement>
-    onHide?: React.MouseEventHandler<HTMLParagraphElement>
-    onScale?: React.MouseEventHandler<HTMLParagraphElement>
     title: string
     width: string
+    draggable?: boolean
+    /**function for closing the window*/
+    onClose?: React.MouseEventHandler<HTMLParagraphElement>
+    /**function for hiding the window */
+    onHide?: React.MouseEventHandler<HTMLParagraphElement>
+    /**function for scaling the window */
+    onScale?: React.MouseEventHandler<HTMLParagraphElement>
+    /**
+     * all drag handlers 
+     *! FIXME: the value should not be undefined 
+     * */
+    dragHandlers: (...args: any[]) => ReactDOMAttributes
 }
 
-const BaseLayoutHeader = ({
-    onClose,
-    onHide,
-    onScale,
-    title,
-    width,
-}: BaseLayoutHeaderProps) => {
+const BaseLayoutHeader = (
+    {
+        title,onClose,onHide,onScale,width,dragHandlers,draggable=true
+    }: BaseLayoutHeaderProps) => {
     return (
-        <div 
-            style={{width: `calc(${width} + 2px)`}}
-            className={styles.header}
-        >
-           <div className={styles.header__buttons}>
+        <div {...dragHandlers()} style={{width: `calc(${width} + 2px)`}} className={styles.header}>
+            <div className={styles.header__buttons}>
                 {onClose && <HeaderCircle color="red" onClick={onClose}/>}
                 {onHide && <HeaderCircle color="green" onClick={onHide}/>}
                 {onScale && <HeaderCircle color="white" onClick={onScale}/>}
-           </div>
-           <div className={styles.header__title}>
+            </div>
+            <div className={styles.header__title}>
                 <Text size="medium 15px">{title}</Text>
-           </div>
-           <div 
-                className={styles.header__nothing}
-           />
+            </div>
+            <div  className={styles.header__nothing} />
         </div>
     );
 };
