228017e9ccf18d0dfc72aa399d0c0faa546ba56c
egorkaBurkenya <sleepyegorka@gmail.com>
2022-11-02 06:16:14 +0300

    remove story ⚠️

--- src/UIKit/Layouts/BaseLayout/BaseLayoutHeader/index.stories.tsx
+++ src/UIKit/Layouts/BaseLayout/BaseLayoutHeader/index.stories.tsx
@@ -7,14 +7,14 @@ export default {
   component: BaseLayoutHeader,
 } as Meta;
 
-export const Default: Story<BaseLayoutHeaderProps> = (args) => <BaseLayoutHeader {...args} />;
-Default.args = {
-  onClose: () => alert("willClose"),
-  onScale: () => alert("willScale"),
-  onHide: () => alert("willHide"),
-  title: "ssh_key worker api loading...",
-  width: "710px",
-  onDrag: () => alert("drag")
-};
+// export const Default: Story<BaseLayoutHeaderProps> = (args) => <BaseLayoutHeader {...args} />;
+// Default.args = {
+//   onClose: () => alert("willClose"),
+//   onScale: () => alert("willScale"),
+//   onHide: () => alert("willHide"),
+//   title: "ssh_key worker api loading...",
+//   width: "710px",
+//   // dragHandlers: 
+// };
 
 
