df9f9c5ea681499bf12b47f8eda2a77ab746f7b4
Nikita Khvatov <nikhvatov19@gmail.com>
2022-12-22 16:25:57 +0300

    setup currect build for npm   ⚠️ without d.ts (type definition files)

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,4 +1,23 @@
 import DefaultButton from '../UIKit/Buttons/DefaultButton';
 import DefaultLongButton from '../UIKit/Buttons/DefaultLongButton';
 
-export { DefaultButton, DefaultLongButton };
+import DropdownIcon from '../UIKit/Dropdown/DropdownIcon'
+import Dropdown from '../UIKit/Dropdown'
+
+import Input from '../UIKit/Input'
+
+import BaseLayout from '../UIKit/Layouts/BaseLayout'
+import NavigateLayout from '../UIKit/Layouts/NavigateLayout'
+
+import ChooseModal from '../UIKit/Modals/ChooseModal'
+import ExtraModal from '../UIKit/Modals/ExtraModal'
+
+import Path from '../UIKit/Path'
+
+import SystemIcon from '../UIKit/SystemIcon'
+
+import Text from '../UIKit/Text'
+
+export { DefaultButton, DefaultLongButton, DropdownIcon, 
+  Dropdown, Input, BaseLayout, NavigateLayout, ChooseModal,
+  ExtraModal, Path, SystemIcon, Text };
